3612d17473aec4950b7ca05dde34cf7a407fad19
Konstantiss <konstantinos.michopoulos@gmail.com>
2024-10-13 16:03:03 +0300

    Add store data in pickle file and plot loss.

--- train_resnet.py
+++ train_resnet.py
@@ -5,10 +5,12 @@ from train_and_validate_model import train_validate
 from dataloader import *
 from resnet import ResNet18
 import time
+import datetime
+import pickle
 import matplotlib.pyplot as plt
 
 BATCH_SIZE = 16
-EPOCHS = 4
+EPOCHS = 1
 LEARNING_RATE = 10**-3
 
 image_transforms = transforms.Compose([
@@ -40,4 +42,56 @@ model = ResNet18().to(device)
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE, weight_decay=0.001, momentum=0.9)
 
+start_time = time.time()
+
 mean_loss_per_epoch_train, mean_loss_per_epoch_val = train_validate(model, train_dataloader, val_dataset, loss_fn, optimizer, device, EPOCHS)
+
+execution_time = (time.time() - start_time) / 60
+date_time = str(datetime.datetime.now())
+model_save_filename = RESULTS_DIR + 'resnet-save-' + date_time + '-' + str(EPOCHS) + '.bin'
+
+
+torch.save(model.state_dict(), model_save_filename)
+
+results = {
+    "model": model.__class__.__name__,
+    "train_loss": mean_loss_per_epoch_train,
+    "val_loss": mean_loss_per_epoch_val,
+    "datetime": datetime.datetime.now(),
+    "execution_time": execution_time
+}
+
+print('Total execution time: {:.4f} minutes', format(execution_time))
+print("Mean training loss per epoch:", mean_loss_per_epoch_train)
+print("Validation loss per epoch:", mean_loss_per_epoch_val)
+
+results_filename = RESULTS_DIR + 'results-resnet-' + date_time + '-' + str(EPOCHS) + '.pkl'
+with open(results_filename, 'wb') as handle:
+    pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+PLOT = True
+
+if PLOT:
+    plot_filename = './figs/resnet-loss-plot-train-' + date_time + '-' + str(EPOCHS) + '.png'
+    plt.figure(figsize=(10, 5))
+    plt.title("Resnet training loss per epoch")
+    plt.plot(range(1, EPOCHS + 1), mean_loss_per_epoch_train, linestyle='solid', marker='o', label="Cross Entropy Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    # plt.xlim(1, )
+    # plt.ylim(0, 1)
+    plt.legend()
+    plt.savefig(plot_filename)
+    plt.show()
+
+    plot_filename = './figs/resnet-loss-plot-val-' + date_time + '-' + str(EPOCHS) + '.png'
+    plt.figure(figsize=(10, 5))
+    plt.title("Resnet validation loss per epoch")
+    plt.plot(range(1, EPOCHS + 1), mean_loss_per_epoch_val, linestyle='solid', marker='o', label="Cross Entropy Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    # plt.xlim(1, )
+    # plt.ylim(0, 1)
+    plt.legend()
+    plt.savefig(plot_filename)
+    plt.show()
